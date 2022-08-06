f7b0e7ada0977ccdf264c64f7a39edd3dab88eb8
Jatin-Ya <jatinyadav20032003@gmail.com>
2022-08-06 21:32:29 +0530

    resolved address issue

--- .next/static/webpack/webpack.c8dcbae33fcbb191.hot-update.js
+++ .next/static/webpack/webpack.c8dcbae33fcbb191.hot-update.js
@@ -16,7 +16,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "80a451ed07afd47d"; }
+/******/ 	__webpack_require__.h = function() { return "dcbd4017aebf5e26"; }
 /******/ }();
 /******/ 
 /******/ }
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,13 @@
 import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
-const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface),process.env.FACTORY_ADDRESS);
+let address;
+
+const fetchingAddress = async () => {
+   address = process.env.FACTORY_ADDRESS;
+}
+fetchingAddress();
+
+const instance = new web3.eth.Contract(JSON.parse(CampaignFactory.interface),address);
 
 export default instance;
